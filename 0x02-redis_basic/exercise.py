839dcaa56ab8292d564b21d9561e213f2f797c2e
Lawrence Nno <lawrence.nno@gmail.com>
2023-07-19 15:30:47 +0100

    Added some comments

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Redis module.
-"""
+""" Module for Redis database."""
 import redis
 import uuid
 from functools import wraps
@@ -9,29 +8,24 @@ from typing import Union, Optional, Callable
 
 
 def count_calls(method: Callable) -> Callable:
-    """ Method that count calls.
-    """
+    """ Method that counts num of times cache class are called"""
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **keywords):
-        """ Method wrapper.
-        """
+        """ A wrapper func for call_calls method """
         self._redis.incr(key)
         return method(self, *args, **keywords)
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
-    """ Method that store the history of inputs and outputs
-        for a particular function.
-    """
+    """ Stores a particular func's history of input and outputs """
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args):
-        """ Method wrapper.
-        """
+        """ A wrapper function for call_history method """
         self._redis.rpush("{}:inputs".format(key), str(args))
         history = method(self, *args)
         self._redis.rpush("{}:outputs".format(key),
@@ -41,9 +35,7 @@ def call_history(method: Callable) -> Callable:
 
 
 def replay(method: Callable):
-    """ Method that display the history of calls
-        of a particular function.
-    """
+    """ Displays the history of calls of a particular func """
     r = method.__self__._redis
     keys = method.__qualname__
     inputs = r.lrange("{}:inputs".format(keys), 0, -1)
@@ -56,23 +48,19 @@ def replay(method: Callable):
 
 
 class Cache:
-    """ Cache class.
-    """
+    """ Class for methods that operate a caching system """
     def __init__(self):
-        """ Initializer.
-        """
+        """ Instance of Redis db"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Method that generate a random key,
-            store the input data in Redis using the random key.
-            Arg:
-                data: Can be a str, bytes, int or float.
-            Return:
-                The key as string.
+        """
+        Method takes a data argument and returns a string
+        Generate a random key (e.g. using uuid), store the input data in Redis
+        using the random key and return the key 
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
